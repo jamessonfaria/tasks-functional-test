162f20024ee08ff6210b4906a38223cbcea611c6
Jamesson Junior <jamesson.junior@capco.com>
2023-04-06 13:56:26 -0300

    Ajustes para adicionar o teste para remover tarefa

--- src/test/java/br/com/jamesson/tasks/functional/TasksTest.java
+++ src/test/java/br/com/jamesson/tasks/functional/TasksTest.java
@@ -86,4 +86,25 @@ public class TasksTest {
     }
   }
 
+  @Test
+  public void deveRemoverTarefaComSucesso() {
+    WebDriver driver = accessarAplicacao();
+
+    try {
+      driver.findElement(By.id("addTodo")).click();
+      driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+      driver.findElement(By.id("dueDate")).sendKeys("10/10/2100");
+      driver.findElement(By.id("saveButton")).click();
+      String message = driver.findElement(By.id("message")).getText();
+      Assert.assertEquals("Success!", message);
+
+      driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+      message = driver.findElement(By.id("message")).getText();
+      Assert.assertEquals("Due date must not be in past", message);
+
+    } finally {
+      driver.quit();
+    }
+  }
+
 }
